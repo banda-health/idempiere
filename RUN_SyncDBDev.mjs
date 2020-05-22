ca105769670ed65ba6efe4e1d3091483a6a70970
Kevin <kwburnett@gmail.com>
2020-05-22 16:20:41 +0100

    checking in files that are changed when I run stuff

---
+++ RUN_SyncDBDev.mjs
@@ -0,0 +1,13 @@
+import { exec } from 'child_process';
+
+exec('RUN_SyncDBDev.sh', (error, stdout, stderr) => {
+	if (error) {
+		console.log(`error: ${error.message}`);
+		return;
+	}
+	if (stderr) {
+		console.log(`stderr: ${stderr}`);
+		return;
+	}
+	console.log(`stdout: ${stdout}`);
+});
